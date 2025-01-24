b65ec524afe4b15d9f324281f674d7fc550944d3
rJohn0 <romeo.john@stu.daviess.kyschools.us>
2025-01-24 15:01:05 +0000

    done

--- Lab16b.java
+++ Lab16b.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//ï¿½ A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -
@@ -17,8 +17,6 @@ public class Lab16b
 		System.out.println("even count = "+test.countEvens());
 		System.out.println("perfect count = "+test.countPerfects()+"\n\n");
 
-
-		//add more test cases
 
 
 	}
